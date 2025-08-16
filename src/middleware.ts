ac83a9c72bad583d5043a7284db7ef64dadfdb2a
sabo <rosinante0932@gmail.com>
2025-08-16 13:37:06 +0700

    update(config): logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,21 @@ export async function onRequest(ctx: APIContext, next: MiddlewareNext) {
         ctx.request.headers.get('Referrer') || // 少数代理会用这个
         '';
 
+    const ref = ctx.request.headers.get('referer') || '';
+    const u = new URL(ref);
+    const origin = u.origin;   // https://example.com
+    const path = u.pathname; // 跨域通常是 '/'
+
+    const ref1 = ctx.request.headers.get('Referer')
+
+    console.log(ctx.request.headers, 'ctx.request')
+
+    console.log(ref1, 'ref1')
+
+    console.log(origin, 'origin')
+
+    console.log(path, 'path')
+
     console.log(referer, 'referer')
 
     return next()
