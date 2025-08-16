76d5ace91a2df1d67df6bf795c2a91e5b01d81ff
sabo <rosinante0932@gmail.com>
2025-08-16 12:17:54 +0700

    update(dev): 配置更新

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,16 @@
 import type { APIContext, MiddlewareNext } from 'astro'
 export async function onRequest(ctx: APIContext, next: MiddlewareNext) {
     console.log(process.env.BOSS, '222')
+    const ip =
+        ctx.clientAddress ??
+        ctx.request.headers.get('x-forwarded-for')?.split(',')[0].trim() ??
+        ctx.request.headers.get('x-real-ip') ??
+        ctx.request.headers.get('cf-connecting-ip') ??
+        'unknown';
+
+    console.log(ip)
+
+    ctx.locals.ip = ip;
+
     return next()
 }
