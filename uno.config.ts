ef4018c7e6c7607aff36b64f4234dc128ec17c45
sabo <rosinante0932@gmail.com>
2025-08-17 15:26:44 +0700

    配置更新

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, presetMini, presetAttributify, presetIcons, transformerVariantGroup, transformerDirectives } from 'unocss'
+import { defineConfig, presetUno, presetAttributify, presetIcons, transformerVariantGroup, transformerDirectives } from 'unocss'
 
 export default defineConfig({
   shortcuts: {
@@ -13,7 +13,7 @@ export default defineConfig({
       'shadow-[0_4px_16px_0_rgba(82,142,253,0.35)] ' +
       'hover:shadow-[0_8px_20px_0_rgba(82,142,253,0.45)] ' +
       'active:translate-y-0.5',
-    'btn-info': `btn-base px-4 min-w-40 inline-flex items-center justify-center
+     'btn-info': `btn-base px-4 min-w-40 inline-flex items-center justify-center
       h-[56px] bg-white text-[#1F2D3D] font-600 leading-none
       border-2 border-transparent
       transition-all duration-300
@@ -35,7 +35,7 @@ export default defineConfig({
     },
   },
   presets: [
-    presetMini({ theme: { colors: {} } }),
+    presetUno(),
     presetAttributify(),
     presetIcons()
   ],
